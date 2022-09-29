4d79b9d4f8ad29ee2eed37cc67192f641d15f9b2
faisalisal88 <2003.faizalzal@gmail.com>
2022-09-29 11:21:50 +0700

    Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Faisal Fadilla Saya tinggal didaerah Purabaya
+              olahraga yang saya sukai adalah bulutangkis";
+    }
+}
